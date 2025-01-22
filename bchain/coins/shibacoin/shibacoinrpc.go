49e9739210a3a57eaca19cbbccf64984063bc258
Hans <10783567+hans-tm@users.noreply.github.com>
2025-01-22 01:33:48 -0800

    Add shibacoin bchain

---
+++ bchain/coins/shibacoin/shibacoinrpc.go
@@ -0,0 +1,73 @@
+package shibacoin
+
+import (
+	"encoding/json"
+
+	"github.com/golang/glog"
+	"github.com/trezor/blockbook/bchain"
+	"github.com/trezor/blockbook/bchain/coins/btc"
+)
+
+// ShibacoinRPC is an interface to JSON-RPC shibacoind service.
+type ShibacoinRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewShibacoinRPC returns new ShibacoinRPC instance.
+func NewShibacoinRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &ShibacoinRPC{
+		b.(*btc.BitcoinRPC),
+	}
+	s.RPCMarshaler = btc.JSONMarshalerV1{}
+	s.ChainConfig.SupportsEstimateSmartFee = false
+
+	return s, nil
+}
+
+// Initialize initializes ShibacoinRPC instance.
+func (b *ShibacoinRPC) Initialize() error {
+	ci, err := b.GetChainInfo()
+	if err != nil {
+		return err
+	}
+	chainName := ci.Chain
+
+	glog.Info("Chain name ", chainName)
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewShibacoinParser(params, b.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		b.Testnet = false
+		b.Network = "livenet"
+	} else {
+		b.Testnet = true
+		b.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
+
+// GetBlock returns block with given hash.
+func (b *ShibacoinRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
+	var err error
+	if hash == "" {
+		hash, err = b.GetBlockHash(height)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if !b.ParseBlocks {
+		return b.GetBlockFull(hash)
+	}
+	return b.GetBlockWithoutHeader(hash, height)
+}
